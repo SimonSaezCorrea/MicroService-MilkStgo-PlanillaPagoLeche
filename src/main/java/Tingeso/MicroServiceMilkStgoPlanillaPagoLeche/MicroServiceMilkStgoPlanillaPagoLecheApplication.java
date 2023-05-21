c799e59d8ce8173ccf50e95dd4b4889926b06b45
Simon Saez <nayxxaxx789super@gmail.com>
2023-05-21 19:09:06 -0400

    Creación de Proyecto IntelliJ

---
+++ src/main/java/Tingeso/MicroServiceMilkStgoPlanillaPagoLeche/MicroServiceMilkStgoPlanillaPagoLecheApplication.java
@@ -0,0 +1,13 @@
+package Tingeso.MicroServiceMilkStgoPlanillaPagoLeche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceMilkStgoPlanillaPagoLecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceMilkStgoPlanillaPagoLecheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tingeso/MicroServiceMilkStgoPlanillaPagoLeche/MicroServiceMilkStgoPlanillaPagoLecheApplicationTests.java
@@ -0,0 +1,13 @@
+package Tingeso.MicroServiceMilkStgoPlanillaPagoLeche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceMilkStgoPlanillaPagoLecheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
